dc198bfa5e223ab67c88bbf7ad1ac9a2551a3a61
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2024-06-09 11:20:58 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-let connection;
-
-function handleDisconnect() {
-    connection = mysql.createConnection({
+let    connection = mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         port: process.env.DB_PORT
     });
+
+function handleDisconnect() {
+ 
 
     connection.connect(err => {
         if (err) {
