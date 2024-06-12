416ceb81d66d13e007ed9fd89d2902d64c7625c2
Charbel Sonon <charbelsnn@gmail.com>
2024-06-13 00:18:47 +0100

    update

--- config/uploadConfig.js
+++ config/uploadConfig.js
@@ -24,7 +24,7 @@ const upload = multer({
         }
     }),
     fileFilter: (req, file, cb) => {
-        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
             cb(null, true);
         } else {
             cb(new Error(`Invalid file type, only JPEG and PNG is allowed! :${file.mimetype} `), false);
